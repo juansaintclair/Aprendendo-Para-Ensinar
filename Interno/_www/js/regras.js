b9501931148bf051e0b6c226a224895c1188acc4
juansaintclair <juansaintclair@gmail.com>
2015-06-07 23:59:22 -0300

    :D

--- Interno/_www/js/regras.js
+++ Interno/_www/js/regras.js
@@ -8,8 +8,66 @@
 
 /* global  $, Pace, SelectFx, FForm, classie, DialogFx */
 
+
+//Plugin Click Toggle
+/*
+ * jQuery Function Toggle Pluing
+ * Copyright 2011, Felix Kling
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+;(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}) (function($) {
+    $.fn.funcToggle = function(type, data) {
+        var dname = "jqp_eventtoggle_" + type + (new Date()).getTime(),            
+            funcs = Array.prototype.slice.call(arguments, 2),
+            numFuncs = funcs.length,
+            empty = function() {},
+            false_handler = function() {return false;};
+
+        if(typeof type === "object") {
+            for( var key in type) {
+                $.fn.funcToggle.apply(this, [key].concat(type[key]));
+            }
+            return this;
+        }
+        if($.isFunction(data) || data === false) {
+            funcs = [data].concat(funcs);
+            numFuncs += 1;
+            data = undefined;
+        }
+        
+        funcs = $.map(funcs, function(func) {
+            if(func === false) {
+                return false_handler;
+            }
+            if(!$.isFunction(func)) {
+                return empty;
+            }
+            return func;
+        });
+
+        this.data(dname, 0);
+        this.bind(type, data, function(event) {
+            var data = $(this).data(),
+                index = data[dname];
+            funcs[index].call(this, event);
+            data[dname] = (index + 1) % numFuncs;
+        });
+        return this;
+    };
+});
+
+
+
 $(function () {
-	
+    
 	//Smooth Scroll
 	$('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
@@ -26,7 +84,18 @@ $(function () {
       }
     });
     
-	
+	//Abrir e fechar o menu
+	$('.menu-top').funcToggle('click', function() {
+	   $('.megamenu').fadeIn(200);
+		}, function() {
+		   $('.megamenu').fadeOut(200);
+	});
+
+	//Fechar o menu no mouseOver
+	$('.megamenu').mouseleave(function() {
+	  $(this).fadeOut(200);
+	});
+
 });
 
 (function() {
@@ -36,23 +105,8 @@ $(function () {
 					var svgicon = new svgIcon( el, svgIconConfig );
 				} );
 
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-nav-left-arrow' ), svgIconConfig, { easing : mina.backin } );
+				
 				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-hamburger' ), svgIconConfig, { easing : mina.backin } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-plus' ), svgIconConfig, { easing : mina.backin } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-volume' ), svgIconConfig, { easing : mina.backin } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-hourglass' ), svgIconConfig, { easing : mina.backin } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-equalizer' ), svgIconConfig, { easing : mina.backin } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-hamburger-cross' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-trash' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-clock' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-maximize' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-contract' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
-				new svgIcon( document.querySelector( '.si-icons-easing .si-icon-glass-empty' ), svgIconConfig, { easing : mina.elastic, speed: 600 } );
+})();
 
-				new svgIcon( document.querySelector( '.si-icons-hover .si-icon-clock' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
-				new svgIcon( document.querySelector( '.si-icons-hover .si-icon-hamburger' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
-				new svgIcon( document.querySelector( '.si-icons-hover .si-icon-flag' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
-				new svgIcon( document.querySelector( '.si-icons-hover .si-icon-zoom' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
-				new svgIcon( document.querySelector( '.si-icons-hover .si-icon-maximize' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
-				new svgIcon( document.querySelector( '.si-icons-hover .si-icon-equalizer' ), svgIconConfig, { easing : mina.backin, evtoggle : 'mouseover', size : { w : 128, h : 128 } } );
-			})();
+
