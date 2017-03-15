7f7461ffa6055b8b5277fb296aa0ade3e5552968
Juan Saint Clair <juansaintclair@gmail.com>
2017-03-15 18:37:30 -0300

    primeiro commit

---
+++ js/regras.js
@@ -0,0 +1,19 @@
+/**
+ * @package     Cattive.Site
+ * @subpackage  Templates.cattive
+ *
+ * @copyright   Copyright (C) 2015, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/* global  $, Pace */
+
+$(function () {
+	//Ativando as Tabelas
+	$( "#tabs" ).tabs();
+    $( "#tabs-direita" ).tabs();
+   
+	
+	
+});
+
